46e0a5bb0e41e0343e29717d0313f87238f83fe2
Oskar Radermecker <oskar_radermecker@berkeley.edu>
2019-07-08 11:56:38 -0700

    Major improvements to home page (carousel, featurettes and marketing), team page, about page, added logo support and improved stability overall

--- webpage/home.py
+++ webpage/home.py
@@ -1,31 +1,145 @@
 # -*- coding: utf-8 -*-
-"""
-Éditeur de Spyder
 
-Ceci est un script temporaire.
-"""
-
-from IPython.display import display
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, redirect, url_for, session, logging
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from functools import wraps
 
 app = Flask(__name__)
+app.secret_key = 'secret123'
 
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'asterdb'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# Initialize MySQL
+mysql = MySQL(app)
+
+# Index
 @app.route('/')
+@app.route('/home/')
 def index():
-    return 'This is the home page of home.py. \n The method used is: %s' % request.method
+    return render_template('home.html')
 
-@app.route('/bacon', methods=['GET', 'POST'])
-def bacon():
+# About Aster
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Team
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+# Additional test environment
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+# Register form class
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=2, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.data_required(),
+        validators.equal_to('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+# Register page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))  # encrypt the password
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email,
+                                                                                                  username, password))
+
+        # Commit to db
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+        flash('You are now registered. Log in to access your simulation dashboard!', 'success')
+
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+# Login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     if request.method == 'POST':
-        return 'You are using POST'
-    else:
-        return 'You are probably using GET'
+        # Get form fields
+        username = request.form['username']
+        password_candidate = request.form['password']
 
-@app.route('/profile/<username>')
-def profile(username):
-    return render_template('profile.html', name=username)
+        # Create Cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            # Compare the passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                # Close connection with database
+                cur.close()
+
+                flash('You are now logged in!', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = "Invalid password."
+                return render_template('login.html', error=error)
+        else:
+            error = "Username not found."
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
+
+
+# Check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to log in before you can access your dashboard!', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+
+# User Dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
+
+# User log out page
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('Successfully logged out', 'success')
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
     app.run()
-
