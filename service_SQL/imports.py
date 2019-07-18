11d05225325979eb921253d84dda379d16a8a3e0
Coricos <meryll.dindin@student.ecp.fr>
2019-07-18 11:38:15 -0700

    Reduce micro-service size

--- service_SQL/imports.py
+++ service_SQL/imports.py
@@ -6,20 +6,20 @@ import os
 import uuid
 import json
 import yaml
+import tqdm
+import joblib
 import warnings
+import numpy as np
 
 from flask import Flask
 from flask import abort
 from flask import request
 from flask import Response
+from pqdict import PQDict
 from functools import wraps
 from passlib.hash import sha256_crypt
 from flask_sqlalchemy import SQLAlchemy
 
-import tqdm
-import joblib
-import geopandas
-import numpy as np
-import pandas as pd
-
-from pqdict import PQDict
+# Heavy imports (not suitable for micro-service)
+# import geopandas
+# import pandas as pd
