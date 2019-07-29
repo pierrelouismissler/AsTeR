13330abb969084d1e6d91b18ac1e83e82c99cd9d
Oskar Radermecker <oskar_radermecker@berkeley.edu>
2019-07-28 18:16:09 -0700

    Added table for calls

--- service_WEB/static/includes/simulation.js
+++ service_WEB/static/includes/simulation.js
@@ -1,11 +1,27 @@
-// $('simulation-btn').on('click', function(e) {
+// $('#simulation-btn').on('click', function(e) {
 //
 //     $(this).button('loading');
 //
 // });
+//
+// $(document).ready(function(){
+//   $("simulation-btn").click(function(){
+//     $(this).button('loading');
+//   });
+// });
 
-$(document).ready(function(){
-  $("simulation-btn").click(function(){
-    $(this).button('loading');
-  });
+// function startSimulation(elmnt) {
+//
+//     elmnt.button('loading');
+//     setInterval("update_values();", 3000);
+//     setInterval("display_markers();", 3000);
+// }
+
+$('#myButton').on('click', function () {
+    var $btn = $(this);
+
+    $btn.button('loading');
+    $btn.button('dispose');
+
+    $btn.button('reset')
 });
